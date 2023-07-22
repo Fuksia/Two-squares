ab02864c3000af3f78d1d6ba8b93dee4117aac68
Sara Wong <sara.fuksia@hotmail.com>
2023-07-22 00:25:10 +0000

    Pending changes exported from your codespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
+  exports: [SquareComponent],
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SquareComponent } from '../square/square.component';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent {
+  
+}
